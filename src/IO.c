65ff992029ff679933a7b41f57ee6df16188d7cc
nickfox-taterli <root@taterli.win>
2016-05-31 10:21:47 +0800

    Update to support old Pi.

--- src/IO.c
+++ src/IO.c
@@ -7,8 +7,8 @@
 #include <sys/ioctl.h>
 #include <linux/spi/spidev.h>
 
-#define PI3_PERI_BASE        0x3F000000
-#define GPIO_BASE                (PI3_PERI_BASE + 0x200000) /* GPIO controller */
+#define PI_PERI_BASE             0x20000000
+#define GPIO_BASE                (PI_PERI_BASE + 0x200000) /* GPIO controller */
 
 #include <stdio.h>
 #include <stdlib.h>
